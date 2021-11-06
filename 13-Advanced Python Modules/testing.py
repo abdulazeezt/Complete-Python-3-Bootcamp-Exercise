6f7c06339c4d3b6700f2396c74cadef93512ff0a
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2021-11-06 09:04:22 +0530

    .

---
+++ 13-Advanced Python Modules/testing.py
@@ -0,0 +1,22 @@
+import os
+os.getcwd()
+
+f = open("testfile1.txt",'w+')
+f2 = open("testfile2.txt",'w+')
+
+f.write("A new text has been added to the file1")
+f2.write("A New text has been added to the file2")
+
+f.close()
+f2.close()
+
+import zipfile
+
+#  zipfile.ZipFile.filename.
+
+zipobject = zipfile.ZipFile('comp_file.zip','w')
+
+zipobject.write( "testfile1.txt" , compress_type=zipfile.ZIP_DEFLATED )
+
+zipobject.write( "testfile2.txt" , compress_type=zipfile.ZIP_DEFLATED )
+
