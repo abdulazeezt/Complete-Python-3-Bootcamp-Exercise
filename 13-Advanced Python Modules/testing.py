76ae4eb8c511e207a13dbe41c935780e6f053177
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2021-11-06 09:15:17 +0530

    .

--- 13-Advanced Python Modules/testing.py
+++ 13-Advanced Python Modules/testing.py
@@ -5,18 +5,20 @@ f = open("testfile1.txt",'w+')
 f2 = open("testfile2.txt",'w+')
 
 f.write("A new text has been added to the file1")
-f2.write("A New text has been added to the file2")
+f2.write("A New text has been added to the file2 \n asdfasdfasdfasdfjasdf asdf;lasdkjfasd flasdjfas  \n ")
 
 f.close()
 f2.close()
 
 import zipfile
 
-#  zipfile.ZipFile.filename.
-
 zipobject = zipfile.ZipFile('comp_file.zip','w')
 
 zipobject.write( "testfile1.txt" , compress_type=zipfile.ZIP_DEFLATED )
-
 zipobject.write( "testfile2.txt" , compress_type=zipfile.ZIP_DEFLATED )
+zipobject.close()
+
+zipobject = zipfile.ZipFile('comp_file.zip','r')
+zipobject.extractall("extracted_content")
+zipobject.close()
 
